55c7ee0cbce4db2890d1634cbc134f7a4b3cf15a
zotel1 <zotelsigel@gmail.com>
2024-10-30 16:19:34 -0300

    update, add endpoint get

--- src/server.js
+++ src/server.js
@@ -1,19 +1,45 @@
-const express = require("express");
+import express, {json, urlencoded} from 'express';
+// eslint-dosable-next-line sort-imports
+
+import { desconnect, connectToCollection, generateCodigo } from '../connection_db.js';
+import { Collection } from 'mongodb';
 
 const server = express();
 
-const PORT = 3000;
-const HOST = "127.0.0.1";
+const messageNotFound = JSON.stringify({ message: 'No hay ningun restaurante registrado con ese nombre.' });
+const messageMissingData = JSON.stringify({ message: 'Faltan datos relevantes.' });
+const messageErrorServer = JSON.stringify({ message: 'Se ha generado un error en el servidor.'});
 
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
+// Middlewares
+server.use(json());
+server.use(urlencoded({ extended: true }));
 
-server.get('/restaurantes', (req, res) => {
-    res.status(200).send("OK");
+// Obtener todos los registros de los restaurantes disponibles (metodo GET): Ruta GET http://127.0.0.1:3005/api/v1/restaurantes
+server.get('/api/v1/restaurantes', async (req, res) => {
+    const { borough, cuisine } = req.query;
+    let restaurantes = [];
+
+    try { const collection = await connectToCollection('restaurantes');
+
+        // Filtrar por borough o cuisine, o devolver todos los restaurantes
+        if ( borough) {
+            restaurantes = awair collection.find({ borough }).sort({ name: 1 }).toArray();
+        } else if (cuisine){
+            restaurantes = await collection.find({ cuisine }).sort({ name: 1 }).toArray();
+        } else {
+            restaurantes = await collection.find().toArray();
+        }
+
+        res.status(200).send(JSON.stringify({ payload: restaurantes }));
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send(messageErrorServer);
+    } finally {
+        await desconnect();
+    }
 });
 
-server.post('/restaurantes', (req, res) => {
-    const { color } = req.body
-})
-
-server.listen(PORT, HOST, () => console.log(`Ejecutandose en http://${HOST}:${PORT}/restaurantes`));
+// Método oyente de solicitudes
+server.listen(process.env.SERVER_PORT, process.env.SERVEROST, () => {
+    console.log(`Ejecutandose en http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/api/v1/restaurantes`);
+});
