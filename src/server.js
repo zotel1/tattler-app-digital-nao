ff793208ebbc3872d8a11395237be73981f11fbb
zotel1 <zotelsigel@gmail.com>
2024-10-29 23:43:13 -0300

    update

--- src/server.js
+++ src/server.js
@@ -5,8 +5,15 @@ const server = express();
 const PORT = 3000;
 const HOST = "127.0.0.1";
 
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
 server.get('/restaurantes', (req, res) => {
     res.status(200).send("OK");
 });
+
+server.post('/restaurantes', (req, res) => {
+    const { color } = req.body
+})
 
 server.listen(PORT, HOST, () => console.log(`Ejecutandose en http://${HOST}:${PORT}/restaurantes`));
