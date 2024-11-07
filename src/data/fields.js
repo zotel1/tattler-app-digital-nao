0530df2425a7cfc3620ed68bb985fd5741c9e23d
zotel1 <zotelsigel@gmail.com>
2024-11-07 16:58:19 -0300

    update, add data on fields

---
+++ src/data/fields.js
@@ -0,0 +1,13 @@
+export const BOROUGHS = {
+    QUEENS: 'Queens',
+    MANHATTAN: 'Manhattan',
+    BROOKLYN: 'Brooklyn'
+};
+
+export const CUISINES = {
+    AMERICAN: 'American',
+    ITALIAN: 'Italian',
+    MEXICAN: 'Mexican',
+    JAPANESE: 'Japanese',
+    FRENCH: 'French'
+};
